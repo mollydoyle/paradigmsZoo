b961a1599d990dca9a0815820a39b62028ccc757
mollydoyle <mdoyle22@student11.cse.nd.edu>
2021-02-14 23:38:48 -0500

    startin on zooTest

--- ZooTest.java
+++ ZooTest.java
@@ -1,1 +1,22 @@
-?
+import java.util.Scanner; 
+
+
+public class ZooTest{
+
+	Animal num1 = new Crocodile("Larry"); 
+	Animal num2 = new Tortoise("Ben"); 
+	Animal num3 = new Dog("Stewart"); 
+	Animal num4 = new Lion("Drake"); 
+	Animal num5 = new Eagle("Arthur"); 
+	Animal num6 = new Flamingo("Cody"); 
+
+	zoo.add(num1); 
+	zoo.add(num2); 
+	zoo.add(num3); 
+	zoo.add(num4); 
+	zoo.add(num5); 
+	zoo.add(num6); 
+
+
+	public static void printDescriptions(ArrayList<Animal> zoo){
+		System.out.println(
